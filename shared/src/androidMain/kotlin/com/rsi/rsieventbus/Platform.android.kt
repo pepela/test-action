b3d21feb4eed6e28d3da6da30ba5d27998bbfa09
Giorgi Peranidze <peranidze@rushstreetinteractive.com>
2023-11-14 15:45:45 +0200

    app is more casual

--- shared/src/androidMain/kotlin/com/rsi/rsieventbus/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/rsi/rsieventbus/Platform.android.kt
@@ -1,7 +1,7 @@
 package com.rsi.rsieventbus
 
 class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+    override val name: String = "Android piu view ${android.os.Build.VERSION.SDK_INT}"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
